3878cecb7dc845216ce7926c0780a0ecb2320672
Sanjai R <samsanjai777@gmail.com>
2023-06-24 23:04:14 +0530

    refactor(frontend): updated code

--- pawsome_client/lib/SizeConfig.dart
+++ pawsome_client/lib/SizeConfig.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/widgets.dart';
 
 class SizeConfig {
@@ -8,13 +7,11 @@ class SizeConfig {
   static double? blockSizeHorizontal;
   static double? blockSizeVertical;
 
-
   void init(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
     screenWidth = _mediaQueryData!.size.width;
     screenHeight = _mediaQueryData!.size.height;
     blockSizeHorizontal = screenWidth! / 100;
     blockSizeVertical = screenHeight! / 100;
-
   }
 }
--- pawsome_client/lib/main.dart
+++ pawsome_client/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pawsome_client/router/router.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +15,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF9188e3)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF9188e3),
+        ),
         useMaterial3: true,
         fontFamily: GoogleFonts.poppins().fontFamily,
       ),
